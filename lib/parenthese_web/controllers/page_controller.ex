18b494c2ce5b4e81ebae09c919cc1a76ff1b875d
kimlai <hello@kimlaitrinh.me>
2019-09-19 20:31:01 +0200

    basic index page

--- lib/parenthese_web/controllers/page_controller.ex
+++ lib/parenthese_web/controllers/page_controller.ex
@@ -1,7 +1,10 @@
 defmodule ParentheseWeb.PageController do
   use ParentheseWeb, :controller
 
+  alias Parenthese.Projects
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    projects = Projects.list_projects()
+    render(conn, "index.html", projects: projects)
   end
 end
