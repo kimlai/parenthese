56b7f9aefc473cbbb210faf81463661568dd9bc2
kimlai <hello@kimlaitrinh.me>
2019-11-06 16:21:12 +0100

    try out grid for project details layout

--- assets/js/project.js
+++ assets/js/project.js
@@ -4,15 +4,13 @@ export const seeMore = e => {
   hide(e.target);
   show(document.getElementById("less"));
   const description = document.getElementById("description");
-  removeClass(description, "overflow-y-hidden");
-  removeClass(description, "h-24");
+  addClass(description.parentElement.parentElement, "description-open");
 };
 
 export const seeLess = e => {
   hide(e.target);
   show(document.getElementById("more"));
   const description = document.getElementById("description");
-  addClass(description, "overflow-y-hidden");
-  addClass(description, "h-24");
+  removeClass(description.parentElement.parentElement, "description-open");
   description.scrollIntoView({ block: "nearest" });
 };
