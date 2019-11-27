f7d05621e7bde789be6f8382bfeef52f41dbf71e
kimlai <hello@kimlaitrinh.me>
2019-11-27 15:37:41 +0100

    Fix endpoint configuration
    Ref: https://github.com/phoenixframework/phoenix/blob/6b9e9839b588b6338d1457fd988ec109f605646b/guides/endpoint.md#force-ssl

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :parenthese, ParentheseWeb.Endpoint,
-  url: [scheme: "https", host: nil, port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  url: [scheme: "https", host: "collectifparenthese.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto], host: nil],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
