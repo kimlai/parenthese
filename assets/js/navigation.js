829d8c4b196ca2a34ce4358d49a1b0c88f91437b
kimlai <hello@kimlaitrinh.me>
2020-06-23 09:58:09 +0200

    Do not use AJAX for navigation
    The *only* thing that made us use AJAX for navigation was the page
    transitions and logo animation.
    
    It turns out that the animation can simply be launched when a link a
    clicked, and letting the browser do a full reload. The browser is smart
    enough to not make the UI flash.
    
    The main advantages of this method is that it's must less code, and full
    page reloads are nice when working with javascript. Also, we get scroll
    position management when browsing back pages for free.

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -7,63 +7,17 @@ const navigate = link => {
   removeClass(document.body, "content-loaded");
   addClass(document.body, "content-loading");
   addClass(document.body, "logo-loading");
-
   markActiveLink(link.id);
-
-  document.getElementById("content").innerHTML = "";
-  let contentLoaded = false;
-
-  xhr.abort(); // in case a previous navigation request was sent
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", link.href);
-  xhr.repsonseType = "document";
-  xhr.send();
-  xhr.onload = () => {
-    contentLoaded = true;
-    const page = document.createElement("div");
-    page.innerHTML = xhr.response;
-    const content = page.querySelector("#content");
-    const modal = page.querySelector("#modal");
-    document.getElementById("content").innerHTML = content.innerHTML;
-    document.getElementById("modal").innerHTML = modal.innerHTML;
-    addClass(document.body, "content-loaded");
-    removeClass(document.body, "content-loading");
-    setTimeout(() => {
-      removeClass(document.body, "logo-loading");
-    }, 1200);
-  };
-  setTimeout(() => {
-    if (contentLoaded === true) {
-      removeClass(document.body, "logo-loading");
-    }
-  }, 1200);
 };
 
 const startNavigation = () => {
   document.addEventListener("click", e => {
-    if (!window.history) {
-      return;
-    }
     const link = closestLink(e.target);
     if (!link || link.hasAttribute("data-ignore-navigation")) {
       return;
     }
-    e.preventDefault();
     closeNavbar();
-
-    history.pushState(
-      { activeLinkId: link.id },
-      null,
-      link.getAttribute("href")
-    );
     navigate(link);
-  });
-
-  window.addEventListener("popstate", e => {
-    navigate({
-      href: document.location.pathname,
-      id: e.state && e.state.activeLinkId
-    });
   });
 };
 
